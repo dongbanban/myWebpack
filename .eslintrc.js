1e0566249e3babc26e42623608adcce4ca08e917
dong_yang <dongyang@yeahzee.com>
2021-06-08 14:45:01 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,49 @@
 /*
  * @Author: your name
  * @Date: 2021-06-08 14:12:25
- * @LastEditTime: 2021-06-08 14:20:05
+ * @LastEditTime: 2021-06-08 14:42:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \coded:\myWebpack\.eslintrc.js
  */
 
 module.exports = {
-    root: true,
-    parserOptions: {
-      parser: '@babel/eslint-parser',  // 解析器
-      sourceType: 'module',
-      ecmaVersion: 12
-    },
-    env: {
-      browser: true,
-      node: true,
-      es2021: true
-    },
-    extends: [
-      'airbnb',
-      'eslint:recommended',
-      'plugin:prettier/recommended'
+  root: true,
+  parserOptions: {
+    parser: '@babel/eslint-parser', // 解析器
+    sourceType: 'module',
+    ecmaVersion: 12
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: ['airbnb', 'eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js']
+      }
     ],
-  
-    rules: {
-      'prettier/prettier': 'error'
-    }
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/no-unresolved': [
+      2,
+      {
+        ignore: ['^@/', '^umi/']
+      }
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
+    'no-unused-vars': 0
   }
-  
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-07 10:55:41
- * @LastEditTime: 2021-06-08 14:27:18
+ * @LastEditTime: 2021-06-08 14:43:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \coded:\myWebpack\src\index.js
@@ -12,10 +12,8 @@ import ReactDOM from 'react-dom'
 import './index.less'
 
 const a = 1
-const App = () => 
-{
-  return <div>App134</div>
+const App = () => {
+  return <div>App Test</div>
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
