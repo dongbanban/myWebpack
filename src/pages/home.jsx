2bf88f63eeb9750d23e9fc5da066edcc9a544567
dong_yang <dongyang@yeahzee.com>
2021-07-20 09:51:14 +0800

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-09 17:11:07
- * @LastEditTime: 2021-07-01 11:26:21
+ * @LastEditTime: 2021-07-20 09:50:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \coded:\myWebpack\src\home.jsx
@@ -12,13 +12,6 @@ import { useHistory, useRouteMatch, useParams } from 'react-router-dom'
 import { DatePicker, Space } from 'antd'
 
 export default function Home() {
-  setTimeout(() => {
-    console.log(100)
-  }, 100)
-  setTimeout(() => {
-    console.log(0)
-  }, 0)
-
   const count = useSelector(state => state.counter.value)
   const history = useHistory()
 
