d61805aed3c78c7843466cf5d077985b52f0a052
dongbanban <215387880@qq.com>
2021-08-30 15:09:15 +0800

    update prettier tsconfig.json

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"tabWidth": 2, // 使用2个空格缩进
+	"printWidth": 120, // 超过120个字符就换行
+	"semi": false, // 行尾是否需要分号
+	"singleQuote": true, // 使用单引号
+	"trailingComma": "none", // 末尾不用逗号
+	"bracketSpacing": true, // 大括号内的首尾需要空格
+	"htmlWhitespaceSensitivity": "ignore", // 根据显示样式决定html是否需要折行
+	"jsxBracketSameLine": false, // jsx的反尖括号需要换行
+	"arrowParens": "avoid", // 剪头函数一个参数时不需要括号
+	"requirePragma": false, // 不需要写文件开头的prettier
+	"proseWrap": "preserve" // 使用默认的折行标准
+}
